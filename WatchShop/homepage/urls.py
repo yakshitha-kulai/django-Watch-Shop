2bc57b44b461955b6b2a285754410f5346130742
yakshitha-kulai <yakshitha-kulai>
2024-11-14 22:42:40 +0530

    login and signup created

--- WatchShop/homepage/urls.py
+++ WatchShop/homepage/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Home, About,Upload
+from .views import Home, About,Upload, login_user, logout_user, signup_user
 
 urlpatterns = [
     path('', Home , name ="home"),
     path('about', About , name ="about"),
-    path('upload', Upload, name="upload")
+    path('upload', Upload, name="upload"),
+    path('login', login_user, name="login"),
+    path('logout', logout_user, name="logout"),
+    path('signup', signup_user, name="signup")
+
 ]
--- WatchShop/homepage/views.py
+++ WatchShop/homepage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Watches, WatchesUploads
 from .forms import UploadForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -13,6 +14,7 @@ def Home(request):
 def About(request):
     return render(request, 'about.html')
 
+@login_required(login_url='login')
 def Upload(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
@@ -25,3 +27,54 @@ def Upload(request):
 
 
     return render(request,'WatchUpload.html', {'form': form} )
+
+
+#LOGIN
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, logout, login
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user_name = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username = user_name, password = password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request,'login.html', {'form': form})
+            
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form} )
+
+
+#SIGNUP
+
+def signup_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')    
+    else:
+        form = UserCreationForm()
+    
+    return render(request,'signup.html', {'form': form})
+
+#LOGOUT
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
+
+
+
